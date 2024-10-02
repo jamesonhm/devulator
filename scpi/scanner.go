9181dc7aef47e35ba43a7085d134b2c70724a1d6
jamesonhm <jamesonhmarshall@gmail.com>
2024-10-02 06:50:26 -0600

    add first token types and enum

--- scpi/scanner.go
+++ scpi/scanner.go
@@ -1,5 +1,15 @@
 package scpi
 
+type Scanner struct {
+	start   *rune
+	current *rune
+	line    uint
+}
+
+func InitScanner(source string) {
+
+}
+
 func Scan(src string) []string {
-    return []string{"from", "the", "scanner"}
+	return []string{"from", "the", "scanner"}
 }
--- scpi/tokens.go
+++ scpi/tokens.go
@@ -1,7 +1,40 @@
 package scpi
 
 type Token struct {
-	Name      string
-	TokenType string
-	Value     string
+	tokenType TokenType
+	start     int
+	length    int
+	line      int
+}
+
+type TokenType int
+
+const (
+	COLON TokenType = iota
+	COMMA
+	WHITE_SPACE
+	SEMICOLON
+	DQUOTE
+	PLUS
+	MINUS
+	DOT
+	NUMBER
+	NODE
+	QUERY
+	BANG
+	AT
+	HASH
+	DOLLAR
+	PERCENT
+	CARROT
+	LEFT_PAREN
+	RIGHT_PAREN
+)
+
+func (tt TokenType) TokenString() string {
+	return [...]string{":", ",", " ", ";", "\"", "+"}[tt-1]
+}
+
+func (tt TokenType) EnumIndex() int {
+	return int(d)
 }
